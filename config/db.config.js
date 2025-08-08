4ea9b1006df964b6ff03acba7c620c2eec39cedb
WilVL <120687974+WilVL@users.noreply.github.com>
2025-08-08 02:05:53 -0700

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'previse',
-    port: 3307,
+    host: '',
+    user: '',
+    password: '',
+    database: '',
+    port: '',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
